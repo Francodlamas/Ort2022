5e7967719dceda38adf8be9c29f289493566330e
FRANCO DAMIAN <46647617@ort>
2022-04-04 09:08:55 -0300

    decimo commit

--- ejercicio.js
+++ ejercicio.js
@@ -1,7 +1,7 @@
 // De la lista 11, 33, 2, -1, 110, 99, 8 obtener el segundo mayor impar
-{
+
     const lista1 = [11, 33, 2, -1, 110, 99, 8];
-    lista1.sort();
+    lista1.sort((a, b) => a - b);
     lista1.reverse();
     var a=0;
     for (var i=0; i<lista1.length; i++) 
@@ -15,4 +15,3 @@
         }
         
     }
-}
