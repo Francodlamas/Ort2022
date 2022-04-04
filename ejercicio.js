33947db1600c476f6e9fa4ded3a4cebd78bb544c
FRANCO DAMIAN <46647617@ort>
2022-04-04 09:06:15 -0300

    decimo commit

--- ejercicio.js
+++ ejercicio.js
@@ -2,20 +2,17 @@
 {
     const lista1 = [11, 33, 2, -1, 110, 99, 8];
     lista1.sort();
-lista1.reverse();
-var a=0;
+    lista1.reverse();
+    var a=0;
     for (var i=0; i<lista1.length; i++) 
     { 
-       
+
         if(lista1[i]%2!=0 && a<2 ){
             if(a==1){
         console.log(lista1[i]);
             }
         a++;
         }
+        
     }
-
-
-
-
 }
